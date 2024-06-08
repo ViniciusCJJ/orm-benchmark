c23281a04f00375d9ea6fc984feeb450c5e591ed
duduu202 <Edu.Ap.Oliveira2002@gmail.com>
2024-06-08 14:58:34 -0300

    fix: body params

--- bench.js
+++ bench.js
@@ -4,7 +4,7 @@ import http from "k6/http";
 let body_datas = __ENV.DATA_FILE
   ? JSON.parse(open(String(__ENV.DATA_FILE)))
   : [];
-// para ids
+
 let path_vars = __ENV.PATH_VARS_FILE
   ? JSON.parse(open(String(__ENV.PATH_VARS_FILE)))
   : [];
@@ -14,16 +14,17 @@ export const options = {
   iterations: __ENV.ITERATIONS,
 };
 
+const selectedHost = __ENV.HOST;
+const url = `${selectedHost}/${__ENV.ENDPOINT}`;
+
 export default function () {
-  const selectedHost = __ENV.HOST;
-
-  const url = `${selectedHost}/${__ENV.ENDPOINT}`;
   const path_var = path_vars[scenario.iterationInTest % path_vars.length];
-  const formated_url = url + (path_var ? "/" + path_var : "");
 
-  const body = body_datas[scenario.iterationInTest % body_datas.length];
+  let body =
+    JSON.parse(body_datas[scenario.iterationInTest % body_datas.length]) || {};
+  body.id = path_var;
 
-  http[String(__ENV.HTTP_METHOD).toLowerCase()](formated_url, body, {
+  http[String(__ENV.HTTP_METHOD).toLowerCase()](url, JSON.stringify(body), {
     headers: {
       "Content-Type": "application/json",
     },
